6200e280c36baf3586b1bbb7efda79c5aa88d44a
Grant Simmons <gsimm706@gmail.com>
2021-04-11 02:31:48 -0400

    Clean up main function

--- dsp_test/haar_test.cpp
+++ dsp_test/haar_test.cpp
@@ -48,71 +48,31 @@ int main(int argc, char** argv) {
 
     //Reconstruct Bitmap from Wavelet Decomposition
     std::vector<uint8_t> reconstruction = ihaar2d<double>(image_dequant,image_width,wavelet_block_size,wavelet_block_size);
-    //std::vector<double> test(reconstruction.begin(), reconstruction.end());
-
-    //std::vector<int> diff_vec = diff<int>(reconstruction, reconstruction2);
-    //std::vector<uint8_t> u_diff_vec(diff_vec.begin(), diff_vec.end());
 
     //Display Image
     dump_image(image_pixel_data, "orig.x");
     dump_image(reconstruction, "reconstruction.x");
-    //dump_image(reconstruction2, "reconstruction_no.x");
-    //dump_image(u_diff_vec, "diff.x");
-
-    //int non_zero = print_vector(diff_vec, "DIFF", 512);
-    //std::cout << "NON_ZERO: " << non_zero << std::endl;
-
-    //print_vector(reconstruction, "RECONSTRUCTED IMAGE", 512);
 
     std::cout << (image_pixel_data == reconstruction ? "SUCCESS" : "FAILURE") << std::endl;
 
     std::vector<int> int_transform = haar2d<int>(image_pixel_data,512,8,8);
-/*
-    std::vector<uint8_t> int_reconstruction = ihaar2d<int>(int_transform,512,8,8);
-
-    if(image_pixel_data == int_reconstruction) {
-        std::cout << (image_pixel_data == int_reconstruction ? "SUCCESS" : "FAILURE") << std::endl;
-    }
- */
-    std::vector<uint8_t> v =    {38,30,28,30,98,29,10,111,39,30,28,30,98,29,10,111,
-                                 38,31,28,31,98,29,11,112,39,31,28,31,98,29,11,112,
-                                 38,32,28,32,98,29,12,113,39,32,28,32,98,29,12,113,
-                                 38,33,28,33,98,29,13,114,39,33,28,33,98,29,13,114,
-                                 38,34,28,34,98,29,14,115,39,34,28,34,98,29,14,115,
-                                 38,35,28,35,98,29,15,116,39,35,28,35,98,29,15,116,
-                                 38,36,28,36,98,29,16,117,39,36,28,36,98,29,16,117,
-                                 38,37,28,37,98,29,17,118,39,37,28,37,98,29,17,118,
-                                 37,30,28,30,98,29,10,111,36,30,28,30,98,29,10,111,
-                                 37,31,28,31,98,29,11,112,36,31,28,31,98,29,11,112,
-                                 37,32,28,32,98,29,12,113,36,32,28,32,98,29,12,113,
-                                 37,33,28,33,98,29,13,114,36,33,28,33,98,29,13,114,
-                                 37,34,28,34,98,29,14,115,36,34,28,34,98,29,14,115,
-                                 37,35,28,35,98,29,15,116,36,35,28,35,98,29,15,116,
-                                 37,36,28,36,98,29,16,117,36,36,28,36,98,29,16,117,
-                                 37,37,28,37,98,29,17,118,36,37,28,37,98,29,17,118
-                                 };
-
-    auto v_transform = haar2d<double>(v,16,8,8);
 
     auto test = extract_levels_from_serial<int>(image_quant,512,8,8);
 
-    print_vector(v_transform,"TRANSFORM",16,6);
-
     int initial = 64;
 
-    print_vector(test[0][2],"TEST0D",initial,6);
+    // print_vector(test[0][2],"TEST0D",initial,6);
 
-    auto test_map = generate_frequency_table<int>(image_quant); //Map Wavelet Coefficient Values to the number of their occurrences
-    //auto test_map = generate_frequency_table<int>(test[2][2]);
-    for (auto test : test_map) {
-        std::cout << (int) test.first << ": " << test.second << std::endl;
-    }
+     auto test_map = generate_frequency_table<int>(image_quant); //Map Wavelet Coefficient Values to the number of their occurrences
+    // //auto test_map = generate_frequency_table<int>(test[2][2]);
+    // for (auto test : test_map) {
+    //     std::cout << (int) test.first << ": " << test.second << std::endl;
+    // }
 
     InternalNode* test1 = generate_huffman_tree<int>(test_map); //Generate Huffman encoding based on frequency information
 
     std::map<uint32_t,int> serialize_test = serialize_huffman_tree<int>(test1,0); //Serialize the binary tree into a linear map
 
-    //auto [vec,map,meta] = generate_canonical_huffman_code<int>(serialize_test); //Generate a canonical Huffman code based on the serialized Huffman code bit lengths
     canonical_huffman_table<int> canon = generate_canonical_huffman_code<int>(serialize_test); //Generate a canonical Huffman code based on the serialized Huffman code bit lengths
 
     std::vector<uint32_t> translated = translate_canonical<int,uint32_t>(image_quant, canon.translation_map); //Translate the wavelet coefficients into the new canonical mapping
@@ -122,17 +82,6 @@ int main(int argc, char** argv) {
     std::vector<int> recovered = translate_canonical<uint32_t,int>(translated,inverse); //Generate a fake map reconstruction
 
     std::vector<int> canonical_loss = diff<int,int>(image_quant,recovered);
-    print_vector(canonical_loss,"CANON LOSS",512);
-
-    int count = 0;
-    for(auto val : canonical_loss) {
-        if(val > 0) {
-            count++;
-        }
-    }
-    std::cout << "Diff Count: " << count << std::endl;
-
-    //print_vector(translated,"CANONICAL",512);
 
     for(auto val : serialize_test) {
         std::cout << val.first << " " << val.second << std::endl;
@@ -187,52 +136,5 @@ int main(int argc, char** argv) {
     // print_vector(test[3][1],"TEST3V",initial*4,6);
     // print_vector(test[3][2],"TEST3D",initial*4,6);
 
-/*
-    print_vector(image_pixel_data, "IMAGE", 512);
-    auto test_map = generate_frequency_table<uint8_t>(image_pixel_data);
-    for (auto test : test_map) {
-        std::cout << (int) test.first << ": " << test.second << std::endl;
-    }
-    std::cout << "SIZE: " << test_map.size() << std::endl;
-    std::cout << std::endl;
-
-    print_vector(image_transform, "TRANSFORM", 512);
-
-    auto test_map1 = generate_frequency_table<double>(image_transform);
-    for (auto test : test_map1) {
-        std::cout << (double) test.first << ": " << test.second << std::endl;
-    }
-    std::cout << "SIZE: " << test_map1.size() << std::endl;
-    std::cout << std::endl;
-
-    print_vector(int_transform, "INT TRANSFORM", 512);
-
-    auto test_map2 = generate_frequency_table<int>(int_transform);
-    for (auto test : test_map2) {
-        std::cout << (int) test.first << ": " << test.second << std::endl;
-    }
-    std::cout << "SIZE: " << test_map2.size() << std::endl;
-    std::cout << std::endl;
-/*
-    print_vector(v, "ORIGINAL", 16);
-
-    std::vector<double> v_transform = haar2d<double>(v,16,8,8);
-
-    print_vector(v_transform, "TRANSFROMED", 16);
-
-    const double threshold = 0.2;
-
-    threshold_and_quantize(v_transform,0.2,16,8,8,0.26);
-
-    print_vector(v_transform, "QUANTIZED", 16);
-
-    dequantize(v_transform,0.2,16,8,8);
-    
-    print_vector(v_transform, "DEQUANTIZED", 16);
-
-    std::vector<uint8_t> v_reconstructed = ihaar2d<double>(v_transform,16,8,8);
-
-    print_vector(v_reconstructed, "RECONSTRUCTED", 16);
-*/
     return 0;
 }
